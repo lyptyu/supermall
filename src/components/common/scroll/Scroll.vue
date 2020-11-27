0416350df6e8ffdd60fff7064738c2fe330ba61f
linyipeng <lyptyu@vip.qq.com>
2020-11-27 22:44:46 +0800

    169

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll"
+
+export default {
+  name: "Scroll",
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper,{
+
+    })
+  },
+  data() {
+    return {
+      scroll: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,15 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <div class="wrapper">
-      <div class="content">
-        <home-swiper :banners="banners"></home-swiper>
-        <recommend-view :recommends="recommends"></recommend-view>
-        <feature-view></feature-view>
-        <tab-control class="tab-control" :titles="['流行','新款','精选']"
-                     @tabClick="tabClick"></tab-control>
-        <goods-list :goods="showGoods"></goods-list>
-      </div>
-    </div>
+    <scroll class="content">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" :titles="['流行','新款','精选']"
+                   @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+
   </div>
 </template>
 
@@ -20,13 +19,14 @@
 import HomeSwiper from "@/views/home/childComps/HomeSwiper"
 import RecommendView from "@/views/home/childComps/RecommendView"
 import FeatureView from "@/views/home/childComps/FeatureView"
-import GoodsList from "@/components/content/goods/GoodsList"
+
 
 import NavBar from "@/components/common/navbar/NavBar"
 import TabControl from "@/components/content/tabControl/TabControl"
+import GoodsList from "@/components/content/goods/GoodsList"
+import Scroll from "@/components/common/scroll/Scroll"
 
 import {getHomeMultidata, getHomeGoods} from "network/home"
-
 
 
 export default {
@@ -37,7 +37,8 @@ export default {
     RecommendView,
     FeatureView,
     NavBar,
-    TabControl
+    TabControl,
+    Scroll
   },
   data() {
     return {
@@ -106,6 +107,8 @@ export default {
 <style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
+  position: relative;
 }
 
 .home-nav {
@@ -124,4 +127,20 @@ export default {
   top: 43px;
   z-index: 9;
 }
+
+.content {
+  /*height: 300px;*/
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+
+/*.content{*/
+/*  margin-top: 44px;*/
+/*  height: calc(100% - 44px - 49px);*/
+/*  overflow: hidden;*/
+/*}*/
 </style>
