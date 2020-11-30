06176d4f1afba6c11a437e22d39ede5845878fb1
linyipeng <lyptyu@vip.qq.com>
2020-11-30 21:01:15 +0800

    188

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
-    <detail-base-info :goods="goods"></detail-base-info>
-    <detail-shop-info :shop="shop"></detail-shop-info>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo"
+                         @imageLoad="imageLoad"/>
+      <detail-param-info :param-info="paramInfo"/>
+    </scroll>
   </div>
 </template>
 
@@ -12,43 +17,75 @@ import DetailNavBar from "@/views/detail/childComps/DetailNavBar"
 import DetailSwiper from "@/views/detail/childComps/DetailSwiper"
 import DetailBaseInfo from "@/views/detail/childComps/DetailBaseInfo"
 import DetailShopInfo from "@/views/detail/childComps/DetailShopInfo"
+import Scroll from "@/components/common/scroll/Scroll"
+import DetailGoodsInfo from "@/views/detail/childComps/DetailGoodsInfo"
+import DetailParamInfo from "@/views/detail/childComps/DetailParamInfo"
 
-import {getDetail,Goods,Shop} from "@/network/detail"
+import {getDetail, Goods, Shop,GoodsParam} from "@/network/detail"
 
 export default {
   name: "Detail",
-  components:{
+  components: {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
-    DetailShopInfo
+    DetailShopInfo,
+    Scroll,
+    DetailGoodsInfo,
+    DetailParamInfo
   },
   data() {
     return {
       iid: null,
-      topImages:[],
+      topImages: [],
       goods: {},
-      shop:{}
+      shop: {},
+      detailInfo: {},
+      paramInfo:{}
     }
   },
   created() {
     //保存传入的iid
-    this.iid=this.$route.params.iid
+    this.iid = this.$route.params.iid
     //根据iid请求详情数据
-    getDetail(this.iid).then(res=>{
+    getDetail(this.iid).then(res => {
       console.log(res)
       const data = res.result
       this.topImages = data.itemInfo.topImages
       //获取商品信息
-      this.goods=new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
       //获取店铺信息的对象
-      this.shop= new Shop(data.shopInfo)
+      this.shop = new Shop(data.shopInfo)
+      //保存商品详情数据
+      this.detailInfo = data.detailInfo
+      //获取参数信息
+      this.paramInfo=new GoodsParam(data.itemParams.info,data.itemParams.rule)
     })
 
+  },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh()
+    }
   }
 }
 </script>
 
 <style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
 
+.content {
+  height: calc(100% - 44px);
+}
+
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
 </style>
