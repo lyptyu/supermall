d35b54dde8b3f9f04e5da7df83e10c6cbbcb35da
linyipeng <lyptyu@vip.qq.com>
2020-12-04 20:41:13 +0800

    图片懒加载 vue-lazyload

--- src/main.js
+++ src/main.js
@@ -4,13 +4,18 @@ import router from "@/router"
 import store from "@/store"
 import toast from 'components/common/toast'
 import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 
 Vue.config.productionTip = false
 
 Vue.use(toast)
 //解决移动端300ms延迟
 FastClick.attach(document.body)
-
+//使用懒加载插件
+Vue.use(VueLazyLoad,{
+  loading:require('@/assets/img/common/placeholder.png')
+})
 
 Vue.prototype.$bus=new Vue()
 
